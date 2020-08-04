da344965859bb12fb0eddabf86ca858192ac2320
dangthaison91 <dangthaison.91@gmail.com>
2020-08-04 23:05:29 +0700

    Add vued3tree

--- Browser/src/app.js
+++ Browser/src/app.js
@@ -1,35 +1,10 @@
-import 'jqtree'
-import * as blobUtil from 'blob-util'
+import Vue from 'vue'
+import App from './App'
+// import 'bootstrap/dist/css/bootstrap.css'
+// import 'font-awesome/less/font-awesome.less'
 
-require('file-loader?name=[name].[ext]!./index.html');
-
-const sock = new WebSocket("ws://0.0.0.0:8080");
-
-sock.addEventListener("message", e => {
-    if (typeof e.data === "string") {
-        try {
-            let parsedJSON = JSON.parse("[" + e.data + "]");
-            $('#tree').remove()
-            $("<div>", {
-                id: 'tree'
-            }).appendTo('#body');
-            $('#tree').tree({
-                data: parsedJSON,
-                autoOpen: true,
-                onCreateLi: function (node, $li) {
-                    $li.find('.jqtree-element').click(function () {
-                        sock.send(node.name.replace(" (View) ", ""));
-                        return false;
-                    });
-                }
-            });
-        } catch (e) {
-        }
-    } else {
-        const blob = new Blob([e.data], {
-            type: 'image/png'
-        });
-        var blobURL = blobUtil.createObjectURL(blob);
-        document.getElementById('capture').setAttribute('src', blobURL);
-    }
-});
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  render: h => h(App)
+})
--- Browser/webpack.config.js
+++ Browser/webpack.config.js
@@ -4,15 +4,30 @@ const webpack = require('webpack');
 module.exports = {
   mode: 'production',
   entry: './src/app.js',
+  // entry: path.resolve(__dirname, 'src') + '/app.js',
   output: {
     filename: 'bundle.js',
     path: path.join(__dirname, 'public')
   },
-  plugins: [
-    new webpack.ProvidePlugin({
-        $: 'jquery',
-        jQuery: 'jquery',
-        'window.jQuery': 'jquery'
-      })
-  ]
-};
+}
+
+// const { VueLoaderPlugin } = require('vue-loader')
+
+// module.exports = {
+//   mode: 'development',
+//   entry: [
+//     './src/app.js'
+//   ],
+//   module: {
+//     rules: [
+//       {
+//         test: /\.vue$/,
+//         use: 'vue-loader'
+//       }
+//     ]
+//   },
+//   plugins: [
+//     new VueLoaderPlugin()
+//   ]
+// }
+
